ba45ea1b1ee715b19b58712c57d793aace6f8ec1
visvoker <s2089533@gmail.com>
2026-03-22 22:55:53 +0800

    project setup db

---
+++ app/room/[roomCode]/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function RoomPage() {
+  return (
+    <div>
+      <div className="text-red-600">RoomPage</div>
+    </div>
+  );
+}
