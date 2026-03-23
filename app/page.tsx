4b2865b364f43d2be0db85047dabd6a0bb732ef6
visvoker <s2089533@gmail.com>
2026-03-23 16:25:08 +0800

    LobbyEntryPage的基本設計

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { prisma } from "@/lib/db";
+import { LobbyEntryPage } from "@/components/lobby-entry";
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
-  console.log(JSON.stringify(users));
-
   return (
-    <div>
-      <div>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </div>
-      <div className="text-red-600">hello</div>
-      <Button>Click me</Button>
-    </div>
+    <main className="flex items-center justify-center min-h-screen">
+      <LobbyEntryPage />
+    </main>
   );
 }
